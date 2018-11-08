47fedcf1a7e7d84d3a5135f55e8417b38499e13e
WoongPyo <hongup1@naver.com>
2018-11-08 17:44:10 +0900

    namespace

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+namespace AAA
+{
+	namespace BBB
+	{
+		namespace CCC
+		{
+			int num1;
+			int num2;
+		}
+	}
+}
 
 namespace BestComImpl {
 	void SimpleFunc(void);
@@ -10,10 +21,14 @@ namespace ProgComImpl {
 
 int main(void)
 {
+	namespace ABC = AAA::BBB::CCC;
 	using BestComImpl::SimpleFunc;
 	SimpleFunc();
 	ProgComImpl::SimpleFunc();
-
+	ABC::num1 = 20;
+	ABC::num2 = 40;
+	std::cout << ABC::num1 << std::endl;
+	std::cout << ABC::num2 << std::endl;
 	return 0;
 }
 

8ee1efc1ecf0e4e9a7024df18610e3b82655fdca
WoongPyo <hongup1@naver.com>
2018-11-08 12:39:01 +0900

    네임스페이스

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 
-int Adder(int num1 = 1, int num2 = 2);
+namespace BestSomImpl {
+	void SimpleFunc(void)
+	{
+		std::cout << "BestCom의 함수." << std::endl;
+	}
+}
 
+namespace ProgComImpl {
+	void SimpleFunc(void)
+	{
+		std::cout << "ProgCom의 함수." << std::endl;
+	}
+}
 
 int main(void)
 {
-	std::cout << Adder() << std::endl;
-	std::cout << Adder(5) << std::endl;
-	std::cout << Adder(3,5) << std::endl;
+	BestSomImpl::SimpleFunc();
+	ProgComImpl::SimpleFunc();
 
 	return 0;
-}
-
-int Adder(int num1 = 1, int num2 = 2)
-{
-	return num1 + num2;
 }

dce4b4ad79ee310b7bb815793319f054b8c13eee
WoongPyo <hongup1@naver.com>
2018-11-08 12:43:23 +0900

    함수 선언 정의

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 
 namespace BestSomImpl {
-	void SimpleFunc(void)
-	{
-		std::cout << "BestCom의 함수." << std::endl;
-	}
+	void SimpleFunc(void);
 }
 
 namespace ProgComImpl {
-	void SimpleFunc(void)
-	{
-		std::cout << "ProgCom의 함수." << std::endl;
-	}
+	void SimpleFunc(void);
 }
 
 int main(void)
@@ -20,4 +14,14 @@ int main(void)
 	ProgComImpl::SimpleFunc();
 
 	return 0;
+}
+
+void BestSomImpl::SimpleFunc(void)
+{
+	std::cout << "BestCom의 함수." << std::endl;
+}
+
+void ProgComImpl::SimpleFunc(void)
+{
+	std::cout << "ProgCom의 함수." << std::endl;
 }

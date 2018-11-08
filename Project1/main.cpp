81e9888dd77314cf955914b42c280af39e3c06a4
WoongPyo <hongup1@naver.com>
2018-11-08 17:28:00 +0900

    inline

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
+#include <cstring>
+
+//#define SQUARE(x) ((x)*(x))
+
+template <typename T> 
+
+inline T SQUARE(T x)
+{
+	return x * x;
+}
+
+namespace AAA
+{
+	namespace BBB
+	{
+		namespace CCC
+		{
+			int num1;
+			int num2;
+		}
+	}
+}
 
 namespace BestComImpl {
 	void SimpleFunc(void);
@@ -10,10 +32,18 @@ namespace ProgComImpl {
 
 int main(void)
 {
+	std::cout << SQUARE(5) << std::endl;
+	std::cout << SQUARE(5.5) << std::endl;
+
+	/*
+	AAA::BBB::CCC::num1 = 20;
+	AAA::BBB::CCC::num2 = 30;
+
+	using 
 	using BestComImpl::SimpleFunc;
 	SimpleFunc();
 	ProgComImpl::SimpleFunc();
-
+	*/
 	return 0;
 }
 

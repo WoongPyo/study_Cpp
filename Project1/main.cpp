d067c27b021ce374cfda70012816826891831ae9
WoongPyo <hongup1@naver.com>
2018-11-08 12:23:25 +0900

    함수 오버로딩

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,37 +1,24 @@
 #include <iostream>
 
+void MyFunc(void)
+{
+	std::cout << "Myfunc(void) called" << std::endl;
+}
+
+void MyFunc(char c)
+{
+	std::cout << "void MyFunc( " << c << " ) called" << std::endl;
+}
+
+void MyFunc(int a, int b)
+{
+	std::cout << "void MyFunc( " << a << b << " ) called" << std::endl;
+}
+
 int main(void)
 {
-	char name[100];
-	char lang[200];
-
-	std::cout << "이름을 알려주세요. : ";
-	std::cin >> name;
-	
-	std::cout << "좋아하는 언어는? : ";
-	std::cin >> lang;
-
-	std::cout << "내 이름은 " << name << "입니다." << std::endl;
-	std::cout << "제일 좋아하는 언어는 " << lang << "입니다." << std::endl;
-
-	/*
-	int val1;
-	std::cout << "첫번째 숫자를 입력 : ";
-	std::cin >> val1;
-
-	int val2;
-	std::cout << "두번째 숫자를 입력 : ";
-	std::cin >> val2;
-
-	int result = val1 + val2;
-	std::cout << "덧셈 결과 : " << result << std::endl;
-	*/
-	/*
-	int num = 20;
-	std::cout << "Hello World~!!" << std::endl;
-	std::cout << "Hello " << "World~!!" << std::endl;
-	std::cout << num << ' ' << 'A';
-	std::cout << ' ' << 3.14 << std::endl;
-	*/
+	MyFunc();
+	MyFunc('A');
+	MyFunc(12, 13);
 	return 0;
 }

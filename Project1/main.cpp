afabdea6882b9609023b7a6b7c065a44cdbd2a3b
WoongPyo <hongup1@naver.com>
2018-11-08 12:02:53 +0900

    문자 받기

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,6 +2,19 @@
 
 int main(void)
 {
+	char name[100];
+	char lang[200];
+
+	std::cout << "이름을 알려주세요. : ";
+	std::cin >> name;
+	
+	std::cout << "좋아하는 언어는? : ";
+	std::cin >> lang;
+
+	std::cout << "내 이름은 " << name << "입니다." << std::endl;
+	std::cout << "제일 좋아하는 언어는 " << lang << "입니다." << std::endl;
+
+	/*
 	int val1;
 	std::cout << "첫번째 숫자를 입력 : ";
 	std::cin >> val1;
@@ -12,7 +25,7 @@ int main(void)
 
 	int result = val1 + val2;
 	std::cout << "덧셈 결과 : " << result << std::endl;
-
+	*/
 	/*
 	int num = 20;
 	std::cout << "Hello World~!!" << std::endl;

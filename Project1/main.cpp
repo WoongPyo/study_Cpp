708d97ddaa6402e8eb5ea3717f044985bc9e0de6
WoongPyo <hongup1@naver.com>
2018-11-09 10:53:03 +0900

    참조자 이용함수

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,41 +2,23 @@
 
 using namespace std;
 
+void SwapByRef(int &ref1, int &ref2)
+{
+	int temp = ref1;
+	ref1 = ref2;
+	ref2 = temp;
+}
+
 int main(void)
 {
-	//배열요소를 대상으로 한 참조자
-	int arr[3] = { 1, 2, 3 };
-	int &ref1 = arr[0];
-	int &ref2 = arr[1];
-	int &ref3 = arr[2];
+	// 참조자를 이용한 call by reference
+	int val2 = 10;
+	int val3 = 20;
 
-	cout << ref1 << endl;
-	cout << ref2 << endl;
-	cout << ref3 << endl;
+	SwapByRef(val2, val3);
 
-	//일반변수를 대상으로 한 참조자
-	/*
-	int num1 = 1020;
-	int &num2 = num1;
-	int &num3 = num2;
-	int &num4 = num3;
-
-	//int &ref = 20;		참조자는 상수를 선언할 수 없어
-	//int &ref;				참조자 자체 선언 불가
-	//int &ref = NULL;		참고자는 변수 자체의 선언이 불가
-
-	num2 = 3047;
-
-	cout << "Val : " << num1 << endl;
-	cout << "Ref : " << num2 << endl;
-	cout << "Ref num3: " << num3 << endl;
-	cout << "Ref num4: " << num4 << endl;
-
-	cout << "Val : " << &num1 << endl;
-	cout << "Ref : " << &num2 << endl;
-	cout << "Ref num3: " << &num3 << endl;
-	cout << "Ref num4: " << &num4 << endl;
-	*/
+	cout << "val2 : " << val2 << endl;
+	cout << "val3 : " << val3 << endl;
 
 	return 0;
 }

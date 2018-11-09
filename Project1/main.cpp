07fa8548aa2fd31f53a347bb8a49e3f72ffb00ae
WoongPyo <hongup1@naver.com>
2018-11-09 11:00:54 +0900

    반환타입이 참조자.

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -9,9 +9,26 @@ void SwapByRef(int &ref1, int &ref2)
 	ref2 = temp;
 }
 
+int &RefRetFuncOne(int &ref)
+{
+	ref++;
+	return ref;
+}
+
 int main(void)
 {
+	// 반환형이 참조자인 경우.
+	int num2 = 1;
+	int &num3 = RefRetFuncOne(num2); // int &num3 = num2;
+	num2++;
+	num3++;
+
+	cout << "num2  : " << num2 << endl;
+	cout << "num3  : " << num3 << endl;
+
+
 	// 참조자를 이용한 call by reference
+	/*
 	int val2 = 10;
 	int val3 = 20;
 
@@ -19,6 +36,6 @@ int main(void)
 
 	cout << "val2 : " << val2 << endl;
 	cout << "val3 : " << val3 << endl;
-
+	*/
 	return 0;
 }

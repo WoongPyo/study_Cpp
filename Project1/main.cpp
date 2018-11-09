3819e934a0c9d117e29f6830128cdee9fc341d76
WoongPyo <hongup1@naver.com>
2018-11-09 09:54:08 +0900

    참조차의 불가형식

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -9,6 +9,10 @@ int main(void)
 	int &num3 = num2;
 	int &num4 = num3;
 
+	//int &ref = 20;		참조자는 상수를 선언할 수 없어
+	//int &ref;				참조자 자체 선언 불가
+	//int &ref = NULL;		참고자는 변수 자체의 선언이 불가
+
 	num2 = 3047;
 
 	cout << "Val : " << num1 << endl;

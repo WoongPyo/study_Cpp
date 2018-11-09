d4c2af75cdd9b73d5bc08e82091b17331049deb7
WoongPyo <hongup1@naver.com>
2018-11-09 09:51:12 +0900

    추가

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -6,14 +6,21 @@ int main(void)
 {
 	int num1 = 1020;
 	int &num2 = num1;
+	int &num3 = num2;
+	int &num4 = num3;
 
 	num2 = 3047;
 
 	cout << "Val : " << num1 << endl;
 	cout << "Ref : " << num2 << endl;
+	cout << "Ref num3: " << num3 << endl;
+	cout << "Ref num4: " << num4 << endl;
 
 	cout << "Val : " << &num1 << endl;
 	cout << "Ref : " << &num2 << endl;
+	cout << "Ref num3: " << &num3 << endl;
+	cout << "Ref num4: " << &num4 << endl;
+
 
 	return 0;
 }

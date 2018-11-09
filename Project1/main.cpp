026dc9657ca0931e498309f68aad871edd9342e4
WoongPyo <hongup1@naver.com>
2018-11-09 14:10:08 +0900

    클래스 형식 초기화 함수 넣음

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstring>
+
 
 using namespace std;
 
@@ -14,19 +16,39 @@ namespace CAR_CONST
 		BRK_STEP	= 10
 	};
 }
+/*
+	Car 구조체를 class로 변경 및 적용.
+	- 캡슐화(정보은닉, 범위설정)
 
+	- 정보은닉을 위한 접근제어지시자
+	public		: 어디서든 접근 가능
+	private		: 클래스 내에서만 접근 가능
+	protected	: 상속관계에서만 접근 가능
+	
+	- 범위설정
+	개발자가 분석 후 정의하기 나름.
+*/
 
-
-struct Car
+class Car
 {
+private :
 	char gamerID[CAR_CONST::ID_LEN];
 	int fuelGauge;
 	int curSpeed;
 
+public :
+	void InitMembers(const char *ID, int fuel);
 	void ShowCarState();
 	void Accel();
 	void Break();
 };
+
+void Car::InitMembers(const char *ID, int fuel)
+{
+	strcpy(gamerID, ID);
+	fuelGauge = fuel;
+	curSpeed = 0;
+}
 
 void Car::ShowCarState()
 {
@@ -65,14 +87,16 @@ void Car::Break()
 
 int main(void)
 {
-	Car run77 = { "run77", 100 , 0 };
+	Car run77;
+	run77.InitMembers("run77", 100);
 	run77.Accel();
 	run77.Accel();
 	run77.ShowCarState();
 	run77.Break();
 	run77.ShowCarState();
 	
-	Car run88 = { "run88", 100, 0 };
+	Car run88;
+	run88.InitMembers("run88", 100);
 	run88.Accel();
 	run88.Break();
 	run88.ShowCarState();

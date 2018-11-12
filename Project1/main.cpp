58b573a1fbb431c4fec025be4b481b46de516651
WoongPyo <hongup1@naver.com>
2018-11-12 17:21:56 +0900

    객체배열

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -7,8 +7,17 @@ class Person
 private : 
 	char *name = {};
 	int age;
+
 public:
-	//constructor
+	// 매개변수가 없는 constructor
+	Person()
+	{
+		name = NULL;
+		age = 0;
+		cout << "Person() called!" << endl;
+	}
+
+	// 매개변수가 있는 constructor
 	Person(const char *myName, int myAge)
 	{
 		int len = strlen(myName) + 1;
@@ -16,6 +25,14 @@ public:
 		strcpy(name, myName);
 		age = myAge;
 	}
+	//멤버변수 수정 함수
+	void SetPersonInfo(char *myName, int myAge)
+	{
+		name = myName;
+		age = myAge;
+	}
+
+	//출력함수
 	void ShowPersonInfo() const
 	{
 		cout << "이름 : " << name << endl;
@@ -30,14 +47,33 @@ public:
 	}
 };
 
+/*객체 포인터 배열*/
+
 int main(void)
 {
-	Person man1("이동우", 25);
-	Person man2("홍길동", 29);
+	/*객체배열*/
+	Person personArr[3];
+	char namestr[100];
+	char *strptr;
+	int age;
+	int len;
+	
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "이름 : ";
+		cin >> namestr;
+		cout << "나이 : ";
+		cin >> age;
 
-	man1.ShowPersonInfo();
-	man2.ShowPersonInfo();
-
+		len = strlen(namestr) + 1;
+		strptr = new char[len];
+		strcpy(strptr, namestr);
+		personArr[i].SetPersonInfo(strptr, age);
+		cout << endl;
+	}
+	personArr[0].ShowPersonInfo();
+	personArr[1].ShowPersonInfo();
+	personArr[2].ShowPersonInfo();
 
 	return 0;
 }

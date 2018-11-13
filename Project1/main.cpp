ca0d4a0f384dade9dcddf5d790c54e4ac70bdad2
WoongPyo <hongup1@naver.com>
2018-11-13 10:29:36 +0900

    자기참조의이해

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,66 +2,36 @@
 
 using namespace std;
 
-/* this pointer의 활용 */
-class TwoNumber
-{
-private:
-	int num1;
-	int num2;
-public:
-	TwoNumber(int num1, int num2)
-	{
-		this->num1 = num1; //지역변수의 이름과 멤버 변수의 이름이 같은 경우 this pointer를 이용해서 해결
-		this->num2 = num2;
-	}
-	void ShowTwoNumber()
-	{
-		cout << this->num1 << endl;
-		cout << this->num2 << endl;
-	}
-};
-
-/* 
-	this pointer 이해
-	객체 자기자신을 가리키는 용도.
-
-class SoSimple
+/* self_reference 자기 참조의 이해 */
+class SelfRef
 {
 private:
 	int num;
 public:
-	SoSimple(int n) : num(n)
+	SelfRef(int n) : num(n)
 	{
-		cout << "num = " << num << endl;
-		cout << "address = " << this << endl;
+		cout << "객체생성" << endl;
 	}
-	void ShowSimpleData()
+	SelfRef &Adder(int n)
+	{
+		num += n;
+		return *this; // 자기자신을 참조하기 위해 *를 this 앞에 붙임.
+	}
+	SelfRef &ShowNumber()
 	{
 		cout << num << endl;
-	}
-	SoSimple *GetThisPointer()
-	{
-		return this;
+		return *this;
 	}
 };
-*/
+
 int main(void)
 {
-	/* this pointer의 활용 */
-	TwoNumber two(2, 4);
-	two.ShowTwoNumber();
+	/* self_reference 자기 참조의 이해 */
+	SelfRef obj(3);
+	SelfRef &ref = obj.Adder(2);
 
-	/* this pointer 이해
-	SoSimple sim1(100);
-	SoSimple *ptr1 = sim1.GetThisPointer();
-	cout << ptr1 << ", ";
-	ptr1->ShowSimpleData();
-
-	SoSimple sim2(200);
-	SoSimple *ptr2 = sim2.GetThisPointer();
-	cout << ptr2 << ", ";
-	ptr2->ShowSimpleData();
-	*/
+	obj.ShowNumber();
+	ref.ShowNumber();
 
 	return 0;
 }

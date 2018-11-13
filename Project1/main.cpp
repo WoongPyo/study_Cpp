7f43bb8badae94537041f7195ad9668d19808dcb
WoongPyo <hongup1@naver.com>
2018-11-13 12:58:09 +0900

    주석추가

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -29,16 +29,16 @@ private:
 
 public:
 	Checking(Account **firAccountAdr);
-	int CheckingID(int ID, int accountNumber);
+	int CheckingID(int ID);
 	int GetAccountNumber();
-	void SetAccountNumber();
+	void IncreaseAccountNumber();
 	void PrintAccounts();
 	void PrintSelect();
 };
 
 class Print {
 public :
-	int GetSomting2Int(const char *inputStr);
+	int GetInt(const char *inputStr);
 };
 
 int main(void)
@@ -46,33 +46,32 @@ int main(void)
 	Account *accounts[N_ACCOUNT];
 	Checking check(accounts);
 	Print print;
-	int sel;
-	int result;
+
+	int iDbuf;
 	char *namebuf = new char[30];
 	int moneybuf;
 
-	int closeFlag = 0;
-	int idNum;
+	int matchIdOrder;
+	bool finishFlag = false;
 
 	while(1)
 	{
-		check.PrintSelect();
-		sel = print.GetSomting2Int("[선택] : ");
+		check.PrintSelect();											// 선택 메뉴 출력
 
-		switch (sel)
+		switch (print.GetInt("[선택] : "))								// case 선택 하기
 		{
 		case 1:
 			//계좌를 개설
 			cout << endl << "[계좌 개설]" << endl;
-			idNum = print.GetSomting2Int("계좌 ID		: ");
+			iDbuf = print.GetInt("계좌 ID		: ");					// ID를 받음
 
-			if (check.CheckingID(idNum, check.GetAccountNumber()) == NO_MATCH)
+			if (check.CheckingID(iDbuf) == NO_MATCH)					// 공통된 ID가 있는지 확인 이후 계좌개설
 			{
 				cout << "이름		: ";
 				cin >> namebuf;
-				moneybuf = print.GetSomting2Int("입금액		: ");
-				accounts[check.GetAccountNumber()] = new Account(idNum, moneybuf, namebuf);
-				check.SetAccountNumber();
+				moneybuf = print.GetInt("입금액		: ");
+				accounts[check.GetAccountNumber()] = new Account(iDbuf, moneybuf, namebuf);
+				check.IncreaseAccountNumber();							// 계좌개수를 1 더함
 			}
 			else
 			{
@@ -82,53 +81,61 @@ int main(void)
 		case 2:
 			//입금
 			cout << endl << "[입금]" << endl;
-			idNum = print.GetSomting2Int("계좌 ID		: ");
-			result = check.CheckingID(idNum, check.GetAccountNumber());
-			if (result == NO_MATCH)
+			iDbuf = print.GetInt("계좌 ID		: ");
+			matchIdOrder = check.CheckingID(iDbuf);
+			if (matchIdOrder == NO_MATCH)										// 같은 ID를 가진 계좌가 존재하지 않음
 			{
 				cout << "같은 ID가 존재 하지 않습니다." << endl;
 			}
 			else
 			{
-				moneybuf = print.GetSomting2Int("입금액		: ");
-				accounts[result]->InputMoney(moneybuf);
+				moneybuf = print.GetInt("입금액		: ");
+				accounts[matchIdOrder]->InputMoney(moneybuf);					// 지정 계좌에 돈을 입금
 				cout << "입금완료 되었습니다." << endl << endl;
 			}
 			break;
 		case 3:
 			//출금
 			cout << endl << "[출금]" << endl;
-			idNum = print.GetSomting2Int("계좌 ID		: ");
-			result = check.CheckingID(idNum, check.GetAccountNumber());
-			if (result == NO_MATCH)
+			iDbuf = print.GetInt("계좌 ID		: ");
+			matchIdOrder = check.CheckingID(iDbuf);
+			if (matchIdOrder == NO_MATCH)										// 같은 ID를 가진 계좌가 존재하지 않음
 			{
 				cout << "같은 ID가 존재 하지 않습니다." << endl;
 			}
 			else
 			{
-				moneybuf = print.GetSomting2Int("출금액		: ");
-				if (accounts[result]->OutputMoney(moneybuf))
+				moneybuf = print.GetInt("출금액		: ");
+				if (accounts[matchIdOrder]->OutputMoney(moneybuf))			// 지정 계좌에 입력한 돈을 출금가능 확인
 				{
-					std::cout << "출금완료 되었습니다." << std::endl << std::endl;
+					cout << "출금완료 되었습니다." << endl << endl;
 				}
 				else
 				{
-					cout << "출금액이 잔액보다 많습니다." << std::endl << std::endl;
+					cout << "출금액이 잔액보다 많습니다." << endl << endl;
 				}
 			}
 			break;
 		case 4:
 			//계좌정보출력
-			cout << endl << "[전체출력]" << endl;
+			cout << endl << "[전체출력]" << endl;						// 계좌 전부 출력
 			check.PrintAccounts();
 			break;
 		case 5:
 			//프로그램 종료
-			closeFlag = 1;
+			finishFlag = true;
+			for (int i = 0; i < check.GetAccountNumber(); i++)			// 계좌 제거
+			{
+				delete accounts[i];
+			}
+			break;
+		}
+
+		if (finishFlag)
+		{
 			delete[] namebuf;
 			break;
 		}
-		if (closeFlag)		break;
 	}
 	return 0;
 }
@@ -179,7 +186,7 @@ Checking::Checking(Account **firAccountAdr)
 	accountNumber = 0;
 }
 
-int Checking::CheckingID(int ID, int accountNumber)
+int Checking::CheckingID(int ID)
 {
 	for (int i = 0; i < accountNumber; i++)
 	{
@@ -196,7 +203,7 @@ int Checking::GetAccountNumber(void)
 	return accountNumber;
 }
 
-void Checking::SetAccountNumber(void)
+void Checking::IncreaseAccountNumber(void)
 {
 	accountNumber++;
 }
@@ -219,7 +226,7 @@ void Checking::PrintSelect(void)
 	cout << "5. 프로그램 종료" << endl << endl;
 }
 
-int Print::GetSomting2Int(const char *inputStr)
+int Print::GetInt(const char *inputStr)
 {
 	int buf;
 	cout << inputStr;

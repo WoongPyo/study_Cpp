0de94ff9fe6bd6295b09b10c82e7fcb5b9f87ee4
WoongPyo <hongup1@naver.com>
2018-11-14 15:36:27 +0900

    강사님

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -11,74 +11,102 @@ using namespace std;
 - 간단한 인사관리 프로그램을 작성하세요.
 */
 
-class FullTime
-{
-private:
-	char name[20];
-	int pay;
-public:
-	FullTime(const char *name, int pay) : pay(pay)
-	{
-		strcpy(this->name, name);
-	}
-	int GetPay()
-	{
-		return pay;
-	}
-	void ShowName()
-	{
-		cout << "이름 : " << name << endl;
-	}
-	void ShowPay()
-	{
-		cout << "봉급 : " << pay << endl;
-	}
-};
-
+// 데이터 관리 클래스 entity class
 class Employee
 {
 private:
-	FullTime *fulltime[100];
-	int numEmployee;
+	char name[100];
 public:
-	Employee()
+	//생성자
+	Employee(const char *name)
 	{
-		numEmployee = 0;
+		strcpy(this->name, name);
 	}
-	void AddEmployee(FullTime *fulltime)
+	//사원명을 출력하는 함수
+	void ShowYourName() const
 	{
-		this->fulltime[numEmployee] = fulltime;
-		numEmployee++;
+		cout << "name : " << name << endl;
 	}
-	void ShowEmployee()
+	//급여를 반환하는 가상함수
+	virtual int GetSalary() const
 	{
-		for (int i = 0; i < numEmployee; i++)
-		{
-			fulltime[i]->ShowName();
-			fulltime[i]->ShowPay();
-			cout << endl;
-		}
+		return 0;
 	}
-	int MoneyForMonth()
+	// 사원 정보를 출력하는 가상함수
+	virtual void ShowEmployeeInfo() const
+	{}
+};
+
+class PermanentWorker : public Employee
+{
+private:
+	int salary;
+public:
+	//생성자
+	PermanentWorker(const char *name, int money) : Employee(name), salary(money)
+	{}
+	//급여를 반환하는 함수
+	int GetSalary() const
+	{
+		return salary;
+	}
+	//정규직원 정보를 출력하는 함수
+	void ShowEmployeeInfo() const
+	{
+		ShowYourName();
+		cout << "salary : " << GetSalary() << endl << endl;
+	}
+
+};
+
+//기능적 클래스 handler class
+class EmployeeHandler
+{
+private:
+	//다형성
+	Employee *empList[50];
+	int empNum;
+public:
+	//생성자
+	EmployeeHandler() : empNum(0)
+	{}
+	//사원추가기능의 함수
+	void AddEmployee(Employee *emp)
+	{
+		empList[empNum++] = emp;
+	}
+	//전체사원 급여정보를 출력하는 함수
+	void ShowAllSalaryInfo() const
+	{
+		for (int i = 0; i < empNum; i++)
+			empList[i]->ShowEmployeeInfo();
+	}
+	//이번달에 지급해야 할 총급여를 출력하는 함수
+	void ShowTotalSalary() const
 	{
 		int sum = 0;
-		for (int i = 0; i < numEmployee; i++)
-		{
-			sum += fulltime[i]->GetPay();
-		}
-		return sum;
+		for (int i = 0; i < empNum; i++)
+			sum += empList[i]->GetSalary();
+		cout << "Total Salary : " << sum << endl;
+	}
+	//소멸자
+	~EmployeeHandler()
+	{
+		for (int i = 0; i < empNum; i++)
+			delete empList[i];
 	}
 };
 
 int main()
 {
-	FullTime person1("진우", 3000);
-	FullTime person2("지우", 5000);
-	Employee employee;
+	EmployeeHandler handler;
 
-	employee.AddEmployee(&person1);
-	employee.AddEmployee(&person2);
-	employee.ShowEmployee();
-	cout << employee.MoneyForMonth() << endl;
+	handler.AddEmployee(new PermanentWorker("홍길동", 200));
+	handler.AddEmployee(new PermanentWorker("박수남", 300));
+	handler.AddEmployee(new PermanentWorker("아무개", 500));
+	
+	handler.ShowAllSalaryInfo();
+
+	handler.ShowTotalSalary();
 	return 0;
 }

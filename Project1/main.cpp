e055b23ee4b5a19c718bcd87c909a574462a84d8
WoongPyo <hongup1@naver.com>
2018-11-19 11:32:22 +0900

    ㅇㄹ

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,67 +3,25 @@
 
 using namespace std;
 
-/* friend 선언 함수인경우 */
-class Point;
-
-class PointOP
+/* 정적지역변수 C언어 */
+void Counter()
 {
-private:
-	int opCnt;
-public:
-	PointOP() : opCnt(0)
-	{}
-	Point PointAdd(const Point &pnt1, const Point &pnt2);
-	Point PointSub(const Point &pnt1, const Point &pnt2);
-	~PointOP()
-	{
-		cout << "Operation Timmes : " << opCnt << endl;
-	}
-};
-
-class Point
-{
-private:
-	int x;
-	int y;
-public:
-	Point(const int &xpos, const int &ypos) : x(xpos), y(ypos)
-	{}
-	//friend 함수
-	friend Point PointOP::PointAdd(const Point &pnt1, const Point &pnt2);
-	//friend 함수
-	friend Point PointOP::PointSub(const Point &pnt1, const Point &pnt2);
-	//friend 함수 <-- 전역함수를 friend선언
-	friend void ShowPointPos(const Point&);
-};
-
-Point PointOP::PointAdd(const Point &pnt1, const Point& pnt2)
-{
-	opCnt++;
-	return Point(pnt1.x + pnt2.x, pnt1.y + pnt2.y);
+	// 정적지역변수
+	static int cnt;
+	cnt++;
+	cout << "Current cnt : " << cnt << endl;
 }
 
-Point PointOP::PointSub(const Point& pnt1, const Point& pnt2)
-{
-	opCnt++;
-	return Point(pnt1.x - pnt2.x, pnt1.y - pnt2.y);
-}
-
-void ShowPointPos(const Point &pos)
-{
-	cout << "x : " << pos.x << ", ";
-	cout << "y : " << pos.y << endl;
-}
+/*static 변수(클래스 변수)*/
 
 int main()
 {
-	/*friend 선언 - 함수인 경우 */
-	Point pos1(1, 2);
-	Point pos2(2, 4);
-	PointOP op;
+	/* 정적지역변수 */
+	for (int i = 0; i < 10; i++)
+	{
+		Counter();
+	}
 
-	ShowPointPos(op.PointAdd(pos1, pos2));
-	ShowPointPos(op.PointSub(pos1, pos2));
 
 	return 0;
 }

977a3b1bb318154d1a6073e932718d4f7d68baee
WoongPyo <hongup1@naver.com>
2018-11-19 09:56:35 +0900

    ㄴㅁㅁ

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -15,27 +15,28 @@ public:
 		num += n;
 		return *this;
 	}
-	void ShowData() const
+	void SimpleFunc()
 	{
-		cout << "num : " << num << endl;
+		cout << "SimpleFunc : " << num << endl;
+	}
+	void SimpleFunc() const
+	{
+		cout << "const SimpleFunc : " << num << endl;
 	}
 };
 
 int main()
 {
-	/* const 객체 
-		- const 객체는 const 함수만 호출 가능.
-		객체의 멤버변수의 변경을 허용하지 않음.
+	/* const  함수 오버로딩 
+		- 함수 오버로딩 조건에서 const도 포함됨.
 	*/
-	SoSimple generalObj(10);
-	generalObj.AddNum(10);
-	generalObj.ShowData();
+	SoSimple obj1(7);
+	const SoSimple obj2(10);
 
-	const SoSimple constObj(100);
-	constObj.ShowData(); // const 객체는 const 함수만 호출 할 수 있다.
-	//constObj.AddNum(200);
+	obj1.SimpleFunc();
+	obj2.SimpleFunc();
 
-	/* const  함수 오버로딩 */
+
 	/* friend 선언 */
 
 	return 0;

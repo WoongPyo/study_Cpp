7ef71fe3493725448adf1944bfeac63cc6c1e4bd
WoongPyo <hongup1@naver.com>
2018-11-19 11:38:43 +0900

    sdf

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,23 +3,40 @@
 
 using namespace std;
 
-/* 정적지역변수 C언어 */
-void Counter()
+/* static 변수(클래스 변수) */
+
+int simObjCnt = 0;
+int cmxObjCnt = 0;
+
+class SoSimple
 {
-	// 정적지역변수
-	static int cnt;
-	cnt++;
-	cout << "Current cnt : " << cnt << endl;
-}
+public:
+	SoSimple()
+	{
+		simObjCnt++;
+		cout << simObjCnt << "번째 SoSimple 객체" << endl;
+	}
+};
+
+class SoComplx
+{
+public:
+	SoComplx()
+	{
+		cmxObjCnt++;
+		cout << cmxObjCnt << "번째 SoComplx 객체" << endl;
+	}
+};
 
 int main()
 {
-	/* 정적지역변수 */
-	for (int i = 0; i < 10; i++)
-	{
-		Counter();
-	}
+	/* static 변수(클래스 변수) */
+	SoSimple sim1;
+	SoSimple sim2;
 
+	SoComplx com1;
+	SoComplx com2;
+	SoComplx com3;
 
 	return 0;
 }

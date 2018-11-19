193ec4dde5b1882c609ee4cc581b6ddcceae8bce
WoongPyo <hongup1@naver.com>
2018-11-19 10:37:08 +0900

    ㅁㄴ

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,41 +3,58 @@
 
 using namespace std;
 
-class SoSimple
+/* friend 선언 */
+class Girl;
+
+class Boy
 {
 private:
-	int num;
+	int height;
+	friend class Girl;	// private에 있어도 public에 있어도 같은 기능을 한다.
 public:
-	SoSimple(int n) : num(n)
+	Boy(int len) : height(len)
 	{}
-	SoSimple &AddNum(int n)
-	{
-		num += n;
-		return *this;
-	}
-	void SimpleFunc()
-	{
-		cout << "SimpleFunc : " << num << endl;
-	}
-	void SimpleFunc() const
-	{
-		cout << "const SimpleFunc : " << num << endl;
-	}
+	void ShowYourFriendInfo(Girl &frn);
 };
+
+class Girl
+{
+private:
+	char phNum[20];
+public:
+	friend class Boy; // public에 있어도 private에 있어도 같은 기능을 한다.
+	Girl(const char *num)
+	{
+		strcpy(phNum, num);
+	}
+	void ShowYourFriendInfo(Boy &frn);
+};
+
+void Boy::ShowYourFriendInfo(Girl &frn)
+{
+	cout << "Her phone number : " << frn.phNum << endl;
+}
+
+void Girl::ShowYourFriendInfo(Boy &frn)
+{
+	cout << "His Height : " << frn.height << endl;
+}
 
 int main()
 {
-	/* const  함수 오버로딩 
-		- 함수 오버로딩 조건에서 const도 포함됨.
+	/* friend 선언 
+		- friend 선언은 객체지향의 캡슐화에서 정보은닉을 위배하는 문법임.
+		- 따라서, friend 선언은 필요한 상황에서만 사용해야 함.
+		- 연산자 오버로딩에서 전역함수를 선언시 사용.
 	*/
-	SoSimple obj1(7);
-	const SoSimple obj2(10);
+	Boy boy(180);
+	Girl girl("010-1111-1111");
 
-	obj1.SimpleFunc();
-	obj2.SimpleFunc();
+	boy.ShowYourFriendInfo(girl);
+	girl.ShowYourFriendInfo(boy);
 
+	/*friend 선언 - 함수인 경우 */
 
-	/* friend 선언 */
 
 	return 0;
 }

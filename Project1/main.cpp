1469fc7cc115ea5c464d25d0a2b78ef9694dfb59
WoongPyo <hongup1@naver.com>
2018-11-19 16:40:28 +0900

    ㅁㄴㅇ

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -17,34 +17,41 @@ public:
 	}
 
 	// 멤버변수에 의한 단항 연산자 오버로딩
-	
 	Point &operator++()
-	{
-		cout << xpos << endl;
-		Point pos(xpos++, ypos++);
-		return pos;
-		/*
+	{		
 		xpos++;
 		ypos++;
 		return *this;
-		*/
+	}
+
+	// ++ 후위연산자 오버로딩
+	Point operator++(int)
+	{
+		const Point retobj(xpos, ypos);
+		xpos++;
+		ypos++;
+		return retobj;
 	}
 
 	//전역함수에 의한 단항 연산자 오버로딩
 	friend Point &operator--(Point &ref); //내부의 private함수를 사용하기 위해서 friend함수가 필요
+	friend Point operator--(Point &ref, int);
 };
 
 Point &operator--(Point &ref)
 {
-	
-	Point pos(ref.xpos--, ref.ypos--);
-	return pos;
-	
-	/*
 	ref.xpos--;
 	ref.ypos--;
 	return ref;
-	*/
+}
+
+//-- 후위연산자 오버로딩
+Point operator--(Point &ref, int)
+{
+	const Point retobj(ref);
+	ref.xpos--;
+	ref.ypos--;
+	return retobj;
 }
 
 int main()
@@ -53,26 +60,6 @@ int main()
 		-정의
 		연산자가 오버로딩 되면, 피연산자의 종류에 따라서 연산의 방식이 달라지는 것을 말함.
 	*/
-
-	/* 단항 연산자 오버로딩 */
-	Point pos(1, 5);
-	pos.ShowPosition();
-	//++pos; //전위 연산자
-	pos.operator++();	//멤버함수의 경우
-	//operator++(pos);	//전역함수의 경우
-	pos.ShowPosition();
-	--pos; //전위 연산자
-	//pos.oparator--();
-	pos.ShowPosition();
-	operator--(pos);	//전역함수의 경우
-	pos.ShowPosition();
-
-	++(++pos);
-	cout << '2' << endl;
-	pos.ShowPosition();
-	--(--pos);
-	cout << '1' << endl;
-	pos.ShowPosition();
 
 	/*단항 연산자 오버로딩(후위)
 		- ++pos, --pos (전위일때)
@@ -83,6 +70,16 @@ int main()
 		pos.operator++(int); //int가 붙으면 후위 연산자 정의
 		pos.poerator--(int);
 	*/
+
+	Point pos(3, 5);
+	Point cpy;
+//	cpy = pos--; //전역함수
+	cpy.ShowPosition();
+	pos.ShowPosition();
+
+	cpy = pos++; //멤버함수
+	cpy.ShowPosition();
+	pos.ShowPosition();
 
 	return 0;
 }

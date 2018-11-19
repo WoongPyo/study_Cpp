3d3e0869b9c31d5be03ac6d9d7269d09f0bf005c
WoongPyo <hongup1@naver.com>
2018-11-19 12:28:40 +0900

    static변수

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,19 +3,34 @@
 
 using namespace std;
 
-/* const static 변수 */
-class ContryArea
+/* static 함수(클래스 함수) 
+	-선언된 클래스의 모든 객체가 공유.
+	- public로 선언이 되면, 클래스의 이름을 이용해서 호출이 가능.
+	- 객체의 멤버로 존재하는 것이 아님.
+*/
+
+class SoSimple
 {
+private:
+	int num1;
+	static int num2;
 public:
-	const static int RUSSIA = 17100;
-	const static int SOUTH_KOREA = 100;
+	SoSimple(int n) : num1(n)
+	{}
+	//클래스 함수
+	static void Adder(int n)
+	{
+		//num1 += n; //static 변수가 아니기 때문에 static함수 내에서 변경할 수 없음.
+		num2 += n;
+	}
 };
+
+int SoSimple::num2 = 0;
+
 
 int main()
 {
-	/* const static 변수 */
-	cout << "Russia area : " << ContryArea::RUSSIA << endl;
-	cout << "Korea area : " << ContryArea::SOUTH_KOREA << endl;
+	/* static 함수(클래스 함수) */
 
 	return 0;
 }

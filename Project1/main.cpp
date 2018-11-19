27b00ef72714e6ad8919014c2fab888637f15d04
WoongPyo <hongup1@naver.com>
2018-11-19 11:55:50 +0900

    x

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,47 +3,19 @@
 
 using namespace std;
 
-/* static 변수(클래스 변수) */
-
-//int simObjCnt = 0;
-//int cmxObjCnt = 0;
-
-class SoSimple
+/* const static 변수 */
+class ContryArea
 {
-private:
-	static int simObjCnt;
 public:
-	SoSimple()
-	{
-		simObjCnt++;
-		cout << simObjCnt << "번째 SoSimple 객체" << endl;
-	}
+	const static int RUSSIA = 17100;
+	const static int SOUTH_KOREA = 100;
 };
-
-class SoComplx
-{
-private:
-	static int cmxObjCnt;
-public:
-	SoComplx()
-	{
-		cmxObjCnt++;
-		cout << cmxObjCnt << "번째 SoComplx 객체" << endl;
-	}
-};
-
-int SoSimple::simObjCnt = 0;
-int SoComplx::cmxObjCnt = 0;
 
 int main()
 {
-	/* static 변수(클래스 변수) */
-	SoSimple sim1;
-	SoSimple sim2;
-
-	SoComplx com1;
-	SoComplx com2;
-	SoComplx com3;
+	/* const static 변수 */
+	cout << "Russia area : " << ContryArea::RUSSIA << endl;
+	cout << "Korea area : " << ContryArea::SOUTH_KOREA << endl;
 
 	return 0;
 }

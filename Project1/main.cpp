8c4a43d7a2f8e8b666c82870b57fd40e3e866a6d
WoongPyo <hongup1@naver.com>
2018-11-20 12:29:03 +0900

    CONST

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -18,7 +18,10 @@ public:
 	{
 		delete[] arr;
 	}
-	// 배열 인덱스 연산자 오버로딩
+	int GetArrLen() const
+	{
+		return arrLen;
+	}
 	int &operator[](int idx)
 	{
 		//배열의 범위 체크
@@ -30,7 +33,29 @@ public:
 
 		return arr[idx];
 	}
+
+	// 배열 인덱스 연산자 오버로딩 
+	//const키워드가 오버로딩 조건으로 들어감. 외부에서 const 배열을 쓰려면 다시 정의 해야 함.
+	int &operator[](int idx) const
+	{
+		//배열의 범위 체크
+		if (idx < 0 || idx >= arrLen)
+		{
+			cout << "배열의 범위를 벗어났습니다." << endl;
+			exit(1);
+		}
+
+		return arr[idx];
+	}
 };
+
+void ShowAllData(const BoundCheckIntArray &ref)
+{
+	for (int idx = 0; idx < ref.GetArrLen(); idx++)
+	{
+		cout << ref[idx] << endl;
+	}
+}
 
 int main(void)
 {
@@ -45,8 +70,8 @@ int main(void)
 	for (int i = 0; i < 5; i++)
 		arr[i] = (i + 1) * 11;
 
-	for (int i = 0; i < 6; i++)
-		cout << arr[i] << endl;
+	//for (int i = 0; i < 6; i++)
+	//	cout << arr[i] << endl;
 
 	//ShowAllData(arr);
 

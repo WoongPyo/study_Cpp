cfdf932ae3f55ec3c7f12aa850286826e22d0cb0
WoongPyo <hongup1@naver.com>
2018-11-21 10:48:10 +0900

    safd

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,77 +3,34 @@
 
 using namespace std;
 
-class Point
+void Divide(int num1, int num2)
 {
-private:
-	int xPos, yPos;
-	
-	// 기본 대입연산자의 오버로딩을 막는 방법
-	// 다른 함수에서 대입연산자를 쓰지 못하게 하기 위해 넣음.
-	// 동일한 객체(사번 111이라는 객체)가 시스템 내에서 존재하지 않도록 하기 위해서.
-	Point &operator=(const Point &ref);
-
-	//복사생성자를 막는 방법
-	Point(const Point &ref) {}
-
-public:
-	Point(int x=0, int y=0) : xPos(x), yPos(y)
-	{}
-	void ShowData() const
+	try
 	{
-		cout << "X좌표 : " << xPos << " , "
-			<< "Y좌표 : " << yPos << endl;
+		if (num2 == 0)
+			throw 0;
+		cout << "몫 : " << num1 / num2 << endl;
+		cout << "나머지 : " << num1 % num2 << endl;
 	}
-};
-
-class BoundCheckPointArray
-{
-private:
-	Point *arr;
-	int arrLen;
-
-	//복사 생성자 막음
-	BoundCheckPointArray(const BoundCheckPointArray &arr) {}
-
-	//기본대입연산자 오버로딩 막음
-	BoundCheckPointArray &operator=(const BoundCheckPointArray &arr) {}
-
-public:
-	BoundCheckPointArray(int len) : arrLen(len)
+	catch (int expn)
 	{
-		arr = new Point[len];
+		cout << "first catch" << endl;
+		throw;
 	}
-	~BoundCheckPointArray()
-	{
-		delete[] arr;
-	}
-	Point &operator[](int idx)
-	{
-		if (idx < 0 || idx >= arrLen)
-		{
-			cout << "매별의 범위를 벗어났습니다." << endl;
-			exit(1);
-		}
-		return arr[idx];
-	}
-};
+}
 
 int main()
 {
-	/* 객체의 저장을 위한 배열 클래스의 정의 */
-	BoundCheckPointArray arr(3);
-
-	arr[0] = new Point(3, 4); //기본 대입연산자 호출 -> Point객체의 멤버 (xPos,yPos) 0,0을 3,4로 변경??
-	//내부 멤버변수가 2개 생기는 것을 막아야 할 경우 -> 막아야 하는게 맞을 수 있음.
-	arr[1] = Point(5, 6); //기본 대입연산자 호출
-	arr[2] = Point(7, 8); //기본 대입연산자 호출
-
-	for(int i=0; i<3; i++)
-		arr[i].ShowData();
-
-	//배열 복사
-	BoundCheckPointArray cpy(3);
-	cpy = arr;
+	try
+	{
+		Divide(9, 2);
+		Divide(4, 0);
+	}
+	catch (int expn)
+	{
+		cout << "second catch" << endl;
+		cout << "Error Code : " << expn << endl;
+	}
 
 	return 0;
 }

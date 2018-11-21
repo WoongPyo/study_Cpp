ddbc1a51060e4366840e7c81c9131ceac426f63c
WoongPyo <hongup1@naver.com>
2018-11-21 10:22:03 +0900

    aa

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -27,6 +27,10 @@ public:
 	{
 		arr = new Point[len];
 	}
+	~BoundCheckPointArray()
+	{
+		delete[] arr;
+	}
 	Point &operator[](int idx)
 	{
 		if (idx < 0 || idx >= arrLen)
@@ -43,9 +47,9 @@ int main()
 	/* 객체의 저장을 위한 배열 클래스의 정의 */
 	BoundCheckPointArray arr(3);
 
-	arr[0] = Point(3, 4);
-	arr[1] = Point(5, 6);
-	arr[2] = Point(7, 8);
+	arr[0] = Point(3, 4); //기본 대입연산자 호출 -> Point객체의 멤버 (xPos,yPos) 0,0을 3,4로 변경
+	arr[1] = Point(5, 6); //기본 대입연산자 호출
+	arr[2] = Point(7, 8); //기본 대입연산자 호출
 
 	for(int i=0; i<3; i++)
 		arr[i].ShowData();

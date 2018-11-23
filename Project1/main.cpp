6a1f83779d83df08d935a2e365547636708dedc7
WoongPyo <hongup1@naver.com>
2018-11-23 10:05:26 +0900

    asdf

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,14 +1,73 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 using namespace std;
+
+/* 사용자 정의의 String class*/
+
+class String
+{
+private:
+	int len;
+	char *str;
+public:
+	String();					//매개변수가 없는 생성자
+	String(const char *s);		//매개변수가 있는 생성자
+	String(const String &s);	//복사 생성자
+	~String();
+	String &operator=(const String &s);
+	String operator+(const String &s);
+	String &operator+=(const String &s);
+	String &operator==(const String &s);
+};
+
+String::String()
+{
+	len = 0;
+	str = NULL;
+}
+
+//매개변수가 있는 생성자
+String::String(const char *s)
+{
+	len = strlen(s);
+	strcpy(str,s);
+}
+
+//복사 생성자
+String::String(const String &s)
+{
+	len = s.len;
+	strcpy(str, s.str);
+}
+
+//소멸자
+String::~String()
+{
+	delete[] str;
+}
+
+//operator=
+String &String::operator=(const String &s);
+{
+	String buf;
+	buf.len = s.len;
+	strcpy(buf.str, s.str);
+	return buf;
+}
+
+//operator+
+
+//operator+=
+
+//operator==
 
 int main()
 {
 	/* C++ 표준 string class*/
-	string str1 = "I like string class. ";
-	string str2 = "Me too.";
-	string str3 = str1 + str2;
+	String str1 = "I like string class. ";
+	String str2 = "Me too.";
+	String str3 = str1 + str2;
 
 	cout << str1 << endl;
 	cout << str2 << endl;
